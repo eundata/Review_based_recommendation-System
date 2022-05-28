67d40bacd05f5f5ee9c96bdde34c63e5d20f6a5b
Meow =A= <96279383+eundata@users.noreply.github.com>
2022-05-28 17:11:46 +0900

    Update BERT_Model.py

--- BERT_Model.py
+++ BERT_Model.py
@@ -12,17 +12,18 @@ from keras_bert import get_custom_objects
 # The following is coded to load a file in s3 of aws.
 bucket='dice123'
 data_key = 'Books.json'
-data_location = 's3://dice123/Magazine_Subscriptions.json'.format(bucket, data_key)
+data_location = 's3://dice123/Books.json'.format(bucket, data_key)
 df = pd.read_json(data_location, lines=True)
 df1 = pd.DataFrame(df)
 data_Tt=df1[['reviewText','overall']]
 
-# 
+# If overall is 1 to 3, it is negative. If it is 4 to 5, it is positive.
 Re = pd.DataFrame(data_Tt)
 sentiment = {1: 'nagative',2: 'nagative',3: 'nagative',4: 'positive',5: 'positive'}
 Re['sentiment']=Re['overall'].map(sentiment)
 df2 = Re[['reviewText','sentiment']]
 
+# Set up train set and test set
 (x_train, y_train), (x_test, y_test), preproc = text.texts_from_df(train_df = d2, 
                                                                    text_column = 'reviewText',
                                                                    label_columns=['sentiment'],
@@ -31,14 +32,14 @@ df2 = Re[['reviewText','sentiment']]
                                                                    preprocess_mode='bert',
                                                                    val_pct=0.1)
 
+# BERT model training
 model = text.text_classifier(name='bert', train_data = (x_train, y_train) , preproc=preproc, metrics=['accuracy'])
-
 learner = ktrain.get_learner(model = model, 
                              train_data=(x_train, y_train), 
                              val_data=(x_test, y_test), 
                              batch_size=32, 
                              use_multiprocessing = True)
 
-# save model to s3 of aws
+# Save model to s3 of aws
 client_s3 = boto3.client("s3")
 client_s3.download_file("dice123",'Ktrain_Bert_Model.h5', "./Ktrain_Bert_Model.h5")
